d6209e45c0ea68ecbfeb5fa8772cc1074ab86247
ludson96 <ludson_ps27@hotmail.com>
2025-12-04 12:16:37 -0300

    feat: :sparkles: Atualiza serializer de product

--- api/products/serializers.py
+++ api/products/serializers.py
@@ -1,7 +1,15 @@
 from rest_framework import serializers
-from .models import Product
+from .models import Product, Category
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id', 'name']
 
 class ProductSerializer(serializers.ModelSerializer):
+    category_name = serializers.CharField(source='category.name', read_only=True)
+    category = serializers.PrimaryKeyRelatedField(queryset=Category.objects.all(), write_only=True, required=False, allow_null=True)
+
     class Meta:
         model = Product
         fields = [
@@ -9,5 +17,7 @@ class ProductSerializer(serializers.ModelSerializer):
             'name',
             'price',
             'in_stock',
+            'category', 
+            'category_name', 
         ]
-        read_only_fields = ['id']
+        read_only_fields = ['id', 'category_name']
