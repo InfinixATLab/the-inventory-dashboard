98d8d349a315993f9bf7f4d2cfcaaf3deb32b8d3
ludson96 <ludson_ps27@hotmail.com>
2025-12-04 10:59:07 -0300

    feat: :sparkles: Define o modelo Product

--- api/products/models.py
+++ api/products/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=200, help_text="Nome do produto")
+    price = models.DecimalField(max_digits=10, decimal_places=2, help_text="Preço do produto")
+    in_stock = models.BooleanField(default=True, help_text="O produto está disponível em estoque?")
+
+    def __str__(self):
+        return self.name
