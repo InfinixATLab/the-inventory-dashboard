c7586538f550d79f3763f76b5260e16050a1e4fd
ludson96 <ludson_ps27@hotmail.com>
2025-12-04 13:17:15 -0300

    fix: :bug: exibe corretamente a categoria

--- api/products/serializers.py
+++ api/products/serializers.py
@@ -1,22 +1,18 @@
 from rest_framework import serializers
 from .models import Product, Category
 
+
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ['id', 'name']
 
+
 class ProductSerializer(serializers.ModelSerializer):
-    category = serializers.StringRelatedField(read_only=True)
-    category_id = serializers.PrimaryKeyRelatedField(queryset=Category.objects.all(), source='category', write_only=True, required=False, allow_null=True)
+    category_name = serializers.CharField(source='category.name', read_only=True)
 
     class Meta:
         model = Product
-        fields = [
-            'id',
-            'name',
-            'price',
-            'in_stock',
-            'category', 
-            'category_id', 
-        ]
+        fields = ['id', 'name', 'price', 'in_stock', 'category', 'category_name']
+        extra_kwargs = {'category': {'write_only': True}}
+
