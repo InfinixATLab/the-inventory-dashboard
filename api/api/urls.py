f28c0f8b0e1403042bde9a531a79cc7ba8ef101a
Dantedevil10 <jonhjooj2907@gmail.com>
2025-12-04 10:44:12 -0300

    feat: adiciona models, serializers e viewsets do app products

--- api/api/urls.py
+++ api/api/urls.py
@@ -15,8 +15,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+
+from rest_framework import routers
+
+from products.api import viewsets as productViewsets
+
+route = routers.DefaultRouter()
+
+route.register(r'produto',productViewsets.ProdutoViewset, basename="Produto")
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(route.urls)),
 ]
--- api/products/models.py
+++ api/products/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from uuid import uuid4
 
-# Create your models here.
+class Product(models.Model):
+    id = models.UUIDField(primary_key=True,default=uuid4,editable=False)
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    in_stock = models.BooleanField(default=False)
+    category = models.CharField(max_length=50)
+
+
