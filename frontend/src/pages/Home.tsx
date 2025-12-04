81e93eab37db0907d93398f80f6529734d21bb38
Dantedevil10 <jonhjooj2907@gmail.com>
2025-12-04 11:02:39 -0300

    front: adicao de pagina home e modificacoes nos aquivos css

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,21 @@
+export const Home = () => {
+
+    return(
+        <div className="p-4">
+            <header className="bg-orange-500 text-white p-4 rounded mb-4 flex justify-between items-center">
+                <h1 className="text-xl font-bold">Inventório Produtos</h1>
+                <button
+                    className="bg-white text-orange-500 px-4 py-2 rounded hover:bg-gray-100 transition"
+                >
+                    Adicionar Produto
+                </button>
+            </header>
+
+            <main>
+                
+            </main>
+
+            
+        </div>
+    )
+}
