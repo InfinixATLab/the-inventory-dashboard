59d497b7a1e874d592232aeed0ee181a5d62f98f
“Wesley <wesleyfr.sobrinho@gmail.com>
2025-12-04 10:20:57 -0300

    chore(Frontend): limpexa inicial

---
+++ frontend/src/services/api.ts
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+// Local ou em produção
+const API_URL = import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000/api';
+
+export const api = axios.create({
+    baseURL: API_URL,
+    headers: {
+        'Content-Type': 'application/json',
+    },
+});
+
+export interface Product {
+    id: number;
+    name: string;
+    price: string; 
+    in_stock: boolean;
+    category_name?: string;
+}
