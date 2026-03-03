4d0ba871425479fc20a89fbbda6c33f7266bb77d
Matheus Teixeira <149537799+Matheuscrf@users.noreply.github.com>
2026-03-03 20:42:23 -0300

    feat: create products app and register in settings

--- api/api/settings.py
+++ api/api/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'products',
+    'rest_framework',
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
